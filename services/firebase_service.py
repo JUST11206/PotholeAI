0dd815922e0e98e5df976b70baab469e81b3174d
JUST11206 <malikshoaib7983@gmail.com>
2026-09-10 23:35:51 +0530

    Use Firebase environment variables

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,3 +1,4 @@
+import os
 import firebase_admin
 
 from firebase_admin import credentials
@@ -10,9 +11,33 @@ def initialize_firebase():
     if firebase_admin._apps:
         return
 
-    credential = credentials.Certificate(
-        Config.FIREBASE_SERVICE_ACCOUNT
-    )
+    if os.getenv("FIREBASE_PROJECT_ID"):
+
+        private_key = os.getenv(
+            "FIREBASE_PRIVATE_KEY"
+        ).replace("\\n", "\n")
+
+        firebase_config = {
+            "type": "service_account",
+            "project_id": os.getenv(
+                "FIREBASE_PROJECT_ID"
+            ),
+            "private_key": private_key,
+            "client_email": os.getenv(
+                "FIREBASE_CLIENT_EMAIL"
+            ),
+            "token_uri": "https://oauth2.googleapis.com/token"
+        }
+
+        credential = credentials.Certificate(
+            firebase_config
+        )
+
+    else:
+
+        credential = credentials.Certificate(
+            Config.FIREBASE_SERVICE_ACCOUNT
+        )
 
     firebase_admin.initialize_app(
         credential
